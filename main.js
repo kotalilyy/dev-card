52f6ce34e4fe716adafed662ffa797050bc428bc
Dakota Nelson <kotalilyy@gmail.com>
2021-08-21 19:45:55 -0500

    add image

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const card = document.querySelector(".card__inner");
 
 card.addEventListener("click", function (e) {
-  card.classList.toggle('is-flipped');
+  card.classList.toggle("is-flipped");
 });
